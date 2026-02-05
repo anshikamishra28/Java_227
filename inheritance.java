c4136e2f5afad9c363986a48e5a05f9d991f019e
anshikamishra28 <28anshika0605@gmail.com>
2026-02-05 19:29:59 +0530

    inheritance and its types

--- inheritance.java
+++ inheritance.java
@@ -15,7 +15,7 @@ class student extends person{
         System.out.println("rollno : "+rollno);
     }  
 }
-public class inheritence {
+public class inheritance {
     public static void main(String[] args) {
         student s=new student();
         s.name="abc";
